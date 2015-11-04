0e01e376b06489043205959cda83b2f078235f1a
Mossa Hana <mhana1@hawk.iit.edu>
2015-11-03 19:05:38 -0600

    edited install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -16,26 +16,7 @@ sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
 
 sudo mv vendor /var/www/html &> /tmp/movevendor.txt
 
-#create db instance if not exist
-declare -a dbInsatnceARR
-
-mapfile -t dbInstanceARR < <(aws rds describe-db-instances --output json | grep "\"DBInstanceIdentifier" | sed "s/[\"\:\, ]//g" | sed "s/DBInstanceIdentifier//g" )
-
-if [ ${#dbInstanceARR[@]} -gt 0 ]
-   then
-   LENGTH=${#dbInstanceARR[@]}
-
-       for (( i=0; i<${LENGTH}; i++));
-      do
-        if [ ${dbInstanceARR[i]} == "mh-db"]
-        then
-         echo "Database already exists"
-        else
-        echo "creating new database"
-	sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
-        fi
-   done
-fi
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
 
 
 echo "Hello!" > var/tmp/hello.txt
